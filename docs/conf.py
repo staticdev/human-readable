4b2b4a45c291c3bc7be79d2a5c796e3897b5e8b3
staticdev <staticdev-support@protonmail.com>
2021-11-30 19:53:18 +0100

    Use furo Sphinx theme for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,6 @@ copyright = f"{datetime.now().year}, {author}"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx_rtd_theme",
 ]
 autodoc_typehints = "description"
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
